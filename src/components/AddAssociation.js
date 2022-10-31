00da8305a45fef12049bd03d05c6643abb171322
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-31 19:13:03 +0100

    added functionality for association part of signup page

--- src/components/AddAssociation.js
+++ src/components/AddAssociation.js
@@ -1,35 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 function AddAssociation(props) {
-    const [website, setWebsite] = useState("");
-    const [associationType, setAssociationType] = useState("");
-    const [image, setImage] = useState("");
+  const [website, setWebsite] = useState("");
+  const [associationType, setAssociationType] = useState("");
+  const [image, setImage] = useState("");
 
-    const handleWebsite = (e) => setWebsite(e.target.value);
-    const handleAssociationType = (e) => setAssociationType(e.target.value);
-    const handleImage = (e) => setImage(e.target.value);
+  const [associations, setAssociations] = useState(null);
 
-    return (
-        <div>
-            <input 
-                type="text"
-                name="website"
-                value={website}
-                onChange={handleWebsite} 
-            />
-            <select 
-                name="associationType"
-                value={associationType}
-                onChange={handleAssociationType} 
-            />
-            <input 
-                type="text"
-                name="image"
-                value={image}
-                onChange={handleImage} 
-            />
-      </div>
-    )
+  const handleWebsite = (e) => setWebsite(e.target.value);
+  const handleAssociationType = (e) => setAssociationType(e.target.value);
+  const handleImage = (e) => setImage(e.target.value);
+
+  useEffect(() => {
+    axios.get(`${API_URL}/auth/signup/associationtype`).then((response) => {
+      setAssociations(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <label for="website">Association website:</label>
+      <input
+        id="website"
+        type="text"
+        name="website"
+        value={website}
+        onChange={handleWebsite}
+      />
+      <label for="association">What kind of association are you?</label>
+      <select id="association">
+        {associations !== null &&
+          associations.map((item) => (
+            <option key={associationType}>{item}</option>
+          ))}
+      </select>
+      <label for="image">Association image:</label>
+      <input
+        id="image"
+        type="text"
+        name="image"
+        value={image}
+        onChange={handleImage}
+      />
+    </div>
+  );
 }
 
 export default AddAssociation;
