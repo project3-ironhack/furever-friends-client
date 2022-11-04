24255dcc744e9e30d71b9eae1f2d7a69064e85a3
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-11-04 11:31:22 +0100

    added image for homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,12 @@
+import catImage from '../images/close-up-young-woman-kissing-cute-cat.jpg';
+
 function HomePage() {
-    
+    return (
+      <div id="cover">
+        <img src={catImage} alt="cat and woman" />
+      </div>
+    )
   }
+
    
   export default HomePage;
