d1dd587ac945c760af01ca30f7b5cafd30958483
Liz Manresa <manresa.liz@gmail.com>
2023-04-14 11:50:45 +0200

    work in progress pet list

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
+import './PetCard.css';
+import CatDetails from "./CatDetails";
+import DogDetails from "./DogDetails";
  
 // We are deconstructing props object directly in the parentheses of the function
-function PetCard ( { petName, description, _id } ) {
+function PetCard ( { petName,location, _id, ageType, sex, weight } ) {
   
   return (
-    <div className="PetCard card">
-      <Link to={`/pets/${_id}`}>
+    <div class="profile profile-default">
+      <div class="profile__image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/dog.png" alt="Doggo"/></div>
+      <div class="profile__info">
         <h3>{petName}</h3>
-      </Link>
-      <p style={{ maxWidth: "400px" }}>{description} </p>
+        <p class="profile__info__extra">{location}</p>
+      </div>
+      <div class="profile__stats">
+        <p class="profile__stats__title">Age</p>
+        <h5 class="profile__stats__info">{ageType}</h5>
+      </div>
+      <div class="profile__stats">
+        <p class="profile__stats__title">Weight</p>
+        <h5>{weight}kg</h5>
+      </div>
+      <div class="profile__stats">
+        <p class="profile__stats__title">Sex</p>
+        <h5 class="profile__stats__info">{sex}</h5>
+      </div>
+      <div class="profile__cta"><Link to={`/pets/${_id}`} class="button">Adopt {petName}</Link></div>
     </div>
+    
+  
+    
   );
 }
+
  
 export default PetCard;
--- src/pages/PetListPage.js
+++ src/pages/PetListPage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import './PetList.css';
 
 import PetCard from "../components/PetCard";
 
@@ -28,7 +29,6 @@ function PetListPage() {
 
   return (
     <div className="PetListPage">
-     <h1>Look for your pet</h1>
 
       { pets.map((pet) => (
         <PetCard key={pet._id} {...pet} />
